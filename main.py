1d3e9cac79ea9f99f6d0b691ff714927a8c70bde
DivvaDi <ovhcinnikovadiana2004@gmail.com>
2023-03-09 13:35:52 +0300

    Initial commit6ty567

---
+++ main.py
@@ -0,0 +1,21 @@
+from random import randint
+
+def kalk1():
+    tr = 0
+    fl = 0
+    x = int()
+    y = int()
+    while fl < 3:
+        x = randint(1, 100)
+        y = randint(1, 100)
+        print(x, "+", y)
+        n = int(input("ответ:"))
+        if (n == x + y):
+            print("правильно!")
+            tr += 1
+        else:
+            print("ошибка!")
+            fl += 1
+    print("количество верных ответов", tr)
+
+kalk1()
